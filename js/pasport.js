d4d414c0772d8a1d7bd163411e4ebcd833213cef
AdleibaDanakai <145608175+AdleibaDanakai@users.noreply.github.com>
2023-11-29 13:38:22 +0300

    Create pasport.js

---
+++ js/pasport.js
@@ -0,0 +1,39 @@
+function f1() {
+    const p1 = document.getElementsByClassName("block1")[0];
+    p1.innerHTML = "УФМС РОССИИ ПО ГОР. МОСКВА";
+
+    const p2 = document.getElementsByClassName("block4")[0];
+    p2.innerHTML = "Адлейба";
+
+    const p3 = document.getElementsByClassName("block5")[0];
+    p3.innerHTML = "Данакай";
+
+    const p4 = document.getElementsByClassName("block6")[0];
+    p4.innerHTML = "Гивиевич";
+
+    const p5 = document.getElementsByClassName("block7")[0];
+    p5.innerHTML = "муж.";
+
+    const p6 = document.getElementsByClassName("block9")[0];
+    p6.innerHTML = "гор. Москва";
+}
+
+function f2() {
+    const p1 = document.getElementsByClassName("block1")[0];
+    p1.innerHTML = "THE FEDERAL MIGRATION SERVICE OF RUSSIA";
+
+    const p2 = document.getElementsByClassName("block4")[0];
+    p2.innerHTML = "Adleyba";
+
+    const p3 = document.getElementsByClassName("block5")[0];
+    p3.innerHTML = "Danakay";
+
+    const p4 = document.getElementsByClassName("block6")[0];
+    p4.innerHTML = "Givievich";
+
+    const p5 = document.getElementsByClassName("block7")[0];
+    p5.innerHTML = "male";
+
+    const p6 = document.getElementsByClassName("block9")[0];
+    p6.innerHTML = "Moscow";
+}
